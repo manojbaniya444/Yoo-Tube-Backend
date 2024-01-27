136e9823acbfe7928dfb9b2005da44fffb668b37
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-01-27 23:13:17 +0545

    New Schema: Playlist, comment, likes and tweet

---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Like", likeSchema);
